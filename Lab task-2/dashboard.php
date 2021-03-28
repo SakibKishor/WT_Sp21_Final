b1e8f02c2e3250871fef77e119df25bbbf79ac9a
Sakib Hasan <sakishor.h@gmail.com>
2021-03-28 16:36:04 +0600

    Lab task 2

---
+++ Lab task-2/dashboard.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+	<title> Dashboard </title>
+	</head>
+	
+	<body>
+	<center>
+	<div> <a href ="Education.html"> All Student </a></div>
+	<div><a href ="Family Details.html"> Add student </a></div>
+	<div><a href ="Experience.html"> All department </a> </div>
+	<div><a href ="Achievement.html"> Add Department </a></div>
+
+	</center>
+	</body>
+	
+</html>
